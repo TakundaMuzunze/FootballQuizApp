6dafd352c3efb78054134121cd8bedaaa6493e00
Takunda Muzunze <takmuzunze@gmail.com>
2023-06-12 14:54:19 +0100

    Create script.js
    content toggle function

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleRules() {
+    var rulesContent = document.querySelector('.rules-container-content');
+    var quizComponent = document.querySelector('.quiz-component');
+    quizComponent.classList.toggle('show-rules');
+}

d6e9e7449130ef0749be5b76f074d8eeb9d5f62e
Takunda Muzunze <takmuzunze@gmail.com>
2023-06-12 15:19:00 +0100

    Update script.js
    extended function for how to play button

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toggleRules() {
     var rulesContent = document.querySelector('.rules-container-content');
     var quizComponent = document.querySelector('.quiz-component');
+    var newButtonText = document.querySelector(".rules-btn");
+
     quizComponent.classList.toggle('show-rules');
+    rulesContent.classList.toggle('show-rules');
+  
+    if (rulesContent.classList.contains('show-rules')) {
+      newButtonText.innerHTML = 'Back';
+    } else {
+      newButtonText.innerHTML = 'How to play';
+    }
 }
